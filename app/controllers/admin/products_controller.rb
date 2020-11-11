d1fe171d9be6c2d7fa4e04035b0acb03819bace5
Tom Adam <tom.adam21@gmail.com>
2020-11-11 11:36:26 -0800

    http authenticate working

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USER_NAME'], password: Rails.configuration.admin[:admin_password]
 
   def index
     @products = Product.order(id: :desc).all
---
+++ config/initializers/admin.rb
@@ -0,0 +1,4 @@
+Rails.configuration.admin = {
+  :admin_username => ENV['ADMIN_USER_NAME'],
+  :admin_password => ENV['ADMIN_USER_NAME']
+}
